a7d393ba0ef1d652b8fccfddd1373f8b8f8e8b2c
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2022-10-04 18:50:33 -0400

    update login- signup-j job-post

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const Job = require('../models/Job.model');
 
-
-router.get('/jobs/search', (req, res , ) => {
+      // GET route to search for jobs and companies
+router.get('/jobs/search', (req, res ) => {
     
       const {q:query} = req.query;
       console.log(query)
-      Job.find({
-        $or:[
-          {jobTitle:query},
-          {company:query}
-        ]
-      })
+      
+      Job.find({$or:[
+        {jobTitle: {'$regex':query, '$options':'i'}},
+        {company: {'$regex':query, '$options':'i'}}
+      
+      ]})
       . then ( (results) => {
         console.log(results);
         res.render('jobs/job-results', {jobs:results})
@@ -21,6 +21,7 @@ router.get('/jobs/search', (req, res , ) => {
       });
     
 });
+ // GET route to see company details
 
 router.get('/jobs/:jobId', (req, res, ) => {
       
@@ -36,6 +37,11 @@ router.get('/jobs/:jobId', (req, res, ) => {
        });
 });
 
+  // GET route to apply for a specific job
+
+  // router.get('/jobs/apply', (req, res, ) => {
+  //   res.render('jobs/job-apply');
+  // })
 
 
 
